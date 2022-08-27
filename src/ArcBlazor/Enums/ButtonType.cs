5583b6bc608eaaf92d8b71c275e0d118747a422b
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-27 20:07:12 +0530

    Added enums: ButtonType.cs and Size.cs

---
+++ src/ArcBlazor/Enums/ButtonType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ArcBlazor;
+
+public enum ButtonType
+{
+    [Description("button")]
+    Button,
+    [Description("submit")]
+    Submit,
+    [Description("reset")]
+    Reset
+}
---
+++ src/ArcBlazor/Enums/Size.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ArcBlazor;
+
+public enum Size
+{
+    [Description("small")]
+    Small,
+    [Description("medium")]
+    Medium,
+    [Description("large")]
+    Large
+}
