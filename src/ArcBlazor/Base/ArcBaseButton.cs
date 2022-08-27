7247e2770cb7607dd7745f0d01d383024445f788
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2022-08-27 20:08:32 +0530

    Added new BaseComponent classes

---
+++ src/ArcBlazor/Base/ArcBaseButton.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows.Input;
+using ArcBlazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace ArcBlazor;
+
+public abstract class ArcBaseButton : ArcComponentBase
+{
+    /// <summary>
+    /// Potential activation target for this button. This enables RenderFragments with user-defined
+    /// buttons which will automatically activate the intended functionality. 
+    /// </summary>
+    [CascadingParameter] protected IActivatable Activateable { get; set; }
+
+    /// <summary>
+    /// The HTML element that will be rendered in the root by the component
+    /// By default, is a button
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public string HtmlTag { get; set; } = "button";
+
+    /// <summary>
+    /// The button Type (Button, Submit, Refresh)
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public ButtonType ButtonType { get; set; }
+    
+    /// <summary>
+    /// If set to a URL, clicking the button will open the referenced document. Use Target to specify where
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public string Href { get; set; }
+    
+    /// <summary>
+    /// The target attribute specifies where to open the link, if Link is specified. Possible values: _blank | _self | _parent | _top | <i>framename</i>
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public string Target { get; set; }
+
+    /// <summary>
+    /// If true, the button will be disabled.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.Behavior)]
+    public bool Disabled { get; set; }
+    /// <summary>
+    /// Command executed when the user clicks on an element.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public ICommand Command { get; set; }
+
+    /// <summary>
+    /// Command parameter.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.Button.ClickAction)]
+    public object CommandParameter { get; set; }
+
+    /// <summary>
+    /// Button click event.
+    /// </summary>
+    [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    protected async Task OnClickHandler(MouseEventArgs ev)
+    {
+        if (Disabled)
+            return;
+        await OnClick.InvokeAsync(ev);
+        if (Command?.CanExecute(CommandParameter) ?? false)
+        {
+            Command.Execute(CommandParameter);
+        }
+        Activateable?.Activate(this, ev);
+    }
+
+    protected override void OnInitialized()
+    {
+        SetDefaultValues();
+    }
+
+    protected override void OnParametersSet()
+    {
+        //if params change, must set default values again
+        SetDefaultValues();
+    }
+
+    //Set the default value for HtmlTag, Link and Target 
+    private void SetDefaultValues()
+    {
+        if (Disabled)
+        {
+            HtmlTag = "button";
+            Href = null;
+            Target = null;
+            return;
+        }
+
+        // Render an anchor element if Link property is set and is not disabled
+        if (!string.IsNullOrWhiteSpace(Href))
+        {
+            HtmlTag = "a";
+        }
+    }
+
+    protected ElementReference _elementReference;
+
+    public ValueTask FocusAsync() => _elementReference.FocusAsync();
+}
---
+++ src/ArcBlazor/Base/ArcComponentBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ArcBlazor;
+
+public class ArcComponentBase : ComponentBase
+{
+    /// <summary>
+    /// User class names, separated by space.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.ComponentBase.Common)]
+    public string Class { get; set; }
+
+    /// <summary>
+    /// User styles, applied on top of the component's own classes and styles.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.ComponentBase.Common)]
+    public string Style { get; set; }
+
+    /// <summary>
+    /// Use Tag to attach any user data object to the component for your convenience.
+    /// </summary>
+    [Parameter]
+    [Category(CategoryTypes.ComponentBase.Common)]
+    public object Tag { get; set; }
+
+    /// <summary>
+    /// UserAttributes carries all attributes you add to the component that don't match any of its parameters.
+    /// They will be splatted onto the underlying HTML tag.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    [Category(CategoryTypes.ComponentBase.Common)]
+    public Dictionary<string, object> UserAttributes { get; set; } = new Dictionary<string, object>();
+
+    /// <summary>
+    /// If the UserAttributes contain an ID make it accessible for WCAG labelling of input fields
+    /// </summary>
+    public string FieldId => (UserAttributes?.ContainsKey("id") == true ? UserAttributes["id"].ToString() : $"arcinput-{Guid.NewGuid()}");
+}
