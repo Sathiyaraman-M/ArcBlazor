d54caf2a1a8f95868f8ae94fb86fb33934d62279
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-08 10:32:07 +0530

    Completed ArcButton component

--- src/ArcBlazor/Components/Button/ArcButton.razor.cs
+++ src/ArcBlazor/Components/Button/ArcButton.razor.cs
@@ -1,6 +1,39 @@
-﻿namespace ArcBlazor;
+﻿using ArcBlazor.Extensions;
+using ArcBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace ArcBlazor;
 
 public partial class ArcButton : ArcBaseButton
 {
+    private string ClassName => new CssBuilder("btn")
+        .AddClass($"btn-{Color.ToDescriptionString()}")
+        .AddClass("btn-square", Square)
+        .AddClass("btn-pill", Pill)
+        .AddClass("btn-sm", Size == Size.Small)
+        .AddClass("btn-lg", Size == Size.Large)
+        .AddClass("disabled", Disabled)
+        .AddClass(Class)
+        .Build();
     
+    [Parameter]
+    public bool Square { get; set; }
+    
+    [Parameter]
+    public bool Pill { get; set; }
+    
+    [Parameter]
+    public Size Size { get; set; }
+
+    [Parameter] 
+    public Color Color { get; set; } = Color.Primary;
+
+    [Parameter]
+    public string StartIcon { get; set; }
+    
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    [Parameter] 
+    public string EndIcon { get; set; }
 }
