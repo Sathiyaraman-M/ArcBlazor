e311fff246576c884e74a3c7a5c75e06f605ef25
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-08 10:48:33 +0530

    Added new enum: Breakpoint

---
+++ src/ArcBlazor/Enums/Breakpoint.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace ArcBlazor;
+
+public enum Breakpoint
+{
+    [Description("None")]
+    None,
+    [Description("sm")]
+    Sm,
+    [Description("md")]
+    Md,
+    [Description("lg")]
+    Lg,
+    [Description("xl")]
+    Xl,
+    [Description("xxl")]
+    Xxl
+}
