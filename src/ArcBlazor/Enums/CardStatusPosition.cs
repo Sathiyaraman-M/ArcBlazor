858d48bf7e03f06e4fa93cf41a73e780c29eddc9
Sathiyaraman <Sathiyaraman2003@gmail.com>
2022-10-09 10:33:00 +0530

    Added new enum: CardStatusPosition

---
+++ src/ArcBlazor/Enums/CardStatusPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace ArcBlazor;
+
+public enum CardStatusPosition
+{
+    None,
+    [Description("card-status-top")]
+    Top,
+    [Description("card-status-left")]
+    Left
+}
